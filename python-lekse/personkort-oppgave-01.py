5c9dd8ee3d12d38c93714046874d10d06da02c22
maaja <majlyc09@gvgs.no>
2026-09-04 12:49:54 +0200

    opprettet repo

---
+++ python-lekse/personkort-oppgave-01.py
@@ -0,0 +1,5 @@
+navn = input("Hei, hva heter du? ")
+alder = int(input("hvor gammel er du?"))
+
+print(navn)
+print(f"gammel alder{alder}")
---
+++ python-oving/ovings-oppgave-01.py
@@ -0,0 +1,67 @@
+name = "Maja"
+print(name)
+
+alder = 17
+hoyde = 1.70
+liker_python = True
+print(name)
+print(alder)
+print(hoyde)
+print(liker_python)
+
+print(type(name))
+print(type(alder))
+print(type(hoyde))
+print(type(liker_python))
+
+test = "17"
+print(type(test))
+
+pris = 25
+antall = 3  
+total = pris * antall
+print(total)
+
+pris = 30
+antall = 4
+total = pris * antall
+
+if alder >= 18:
+    print("Du er myndig")
+else:
+    print("Du er ikke myndig")
+
+alder = 15
+if alder >= 18:
+    print("Du er myndig")
+else:
+    print("Du er ikke myndig")
+alder = 19
+if alder >= 18:
+    print("Du er myndig")
+else:
+    print("Du er ikke myndig")
+
+if alder < 13:
+    print("barn")
+elif alder < 18:
+    print("ungdom")
+else:
+    print("voksen")
+
+alder = 10
+if alder < 13:
+    print("barn")
+elif alder < 18:
+    print("ungdom")
+else:
+    print("voksen")
+
+alder = 16
+if alder < 13:
+    print("barn")
+elif alder < 18:
+    print("ungdom")
+else:
+    print("voksen")
+    
