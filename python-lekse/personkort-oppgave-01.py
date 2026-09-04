7a184743673a1047ac24f842ca98fafed48b5338
maaja <majlyc09@gvgs.no>
2026-09-04 13:17:50 +0200

    en variable for navn

--- python-lekse/personkort-oppgave-01.py
+++ python-lekse/personkort-oppgave-01.py
@@ -1,5 +1,12 @@
 navn = input("Hei, hva heter du? ")
 alder = int(input("hvor gammel er du?"))
+hoyde = float(input("hvor høy er du?"))
+liker_python = input("liker du python? (ja/nei)")
+
 
 print(navn)
 print(f"gammel alder{alder}")
+print(f"høyde{hoyde}")
+print(f"liker python? {liker_python}")
+
+
