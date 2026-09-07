9f74b4972c8c3a49990929ca19158d477fa38929
maaja <majlyc09@gvgs.no>
2026-09-07 16:09:07 +0200

    Add grading logic and improve output formatting in character assessment script

---
+++ python-lekse/karaktervurdering-oppgave-02.py
@@ -0,0 +1,10 @@
+poeng = int(input("Skriv inn poengsum: "))
+
+if poeng >= 90:
+    print("meget bra")
+elif poeng >= 70:
+    print("bra")
+elif poeng >= 50:
+    print("godkjent")
+else:
+    print("ikke godkjent")
--- python-lekse/personkort-oppgave-01.py
+++ python-lekse/personkort-oppgave-01.py
@@ -3,10 +3,11 @@ alder = int(input("hvor gammel er du?"))
 hoyde = float(input("hvor høy er du?"))
 liker_python = input("liker du python? (ja/nei)")
 
-
 print(navn)
 print(f"gammel alder{alder}")
 print(f"høyde{hoyde}")
 print(f"liker python? {liker_python}")
+print()
+
 
 
