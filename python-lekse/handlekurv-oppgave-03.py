830c98e86eaddd5fe0ae23a664faeda9268b3cf9
maaja <majlyc09@gvgs.no>
2026-09-07 16:37:22 +0200

    Add initial implementation for calculating total prices of energy drinks and baguettes

---
+++ python-lekse/handlekurv-oppgave-03.py
@@ -0,0 +1,11 @@
+Prispåenergidrikke = 25
+Antallenergidrikke = 2
+Prispåbaguetter = 30
+Antallbaguetter = 3
+
+Totalprisenergidrikke = Prispåenergidrikke * Antallenergidrikke
+Totalprisbaguetter = Prispåbaguetter * Antallbaguetter
+Totalpris = Totalprisenergidrikke + Totalprisbaguetter
+print(f"Totalpris energidrikk: {Totalprisenergidrikke}")
+print(f"Totalpris baguetter: {Totalprisbaguetter}")
+print(f"Totalpris: {Totalpris}")
